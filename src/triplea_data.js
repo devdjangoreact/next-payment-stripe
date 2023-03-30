fafb95152b57d441337e04f48d84db299737fe43
devdjangoreact <devdjangoreact@gmail.com>
2023-03-30 15:21:46 +0000

    test

---
+++ src/triplea_data.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function triplea_data() {
+  return {
+    payment_reference: "PLG-571830-PMT",
+    order_currency: "USD",
+    order_amount: 10,
+    expiry_date: "2022-01-26T10:25:58.234Z",
+    notify_secret: "ap3h46KDKggbhnJDR6FgTPe4",
+    access_token: "b8188fa9a35c8a5f79e7197187714e6f34d7f794",
+    token_type: "Bearer",
+    expires_in: 1499,
+    hosted_url:
+      "https://cdn.triple-a.io/widgets/triplea-ecommerce-payment-v1.1/?access_token=b8188fa9a35c8a5f79e7197187714e6f34d7f794&payment_reference=PLG-571830-PMT&select=1",
+  };
+}
